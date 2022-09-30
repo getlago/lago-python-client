b3f51217d8014560f7e76e3444176184905a6c85
LovroColic <lovro.colic.lc@gmail.com>
2022-09-30 12:36:23 +0200

    feat (coupons): add support for new coupon and applied coupon fields (#33)

--- lago_python_client/models/coupon.py
+++ lago_python_client/models/coupon.py
@@ -8,7 +8,11 @@ class Coupon(BaseModel):
     amount_cents: Optional[int]
     amount_currency: Optional[str]
     expiration: Optional[str]
-    expiration_duration: Optional[int]
+    expiration_date: Optional[str]
+    percentage_rate: Optional[float]
+    coupon_type: Optional[str]
+    frequency: Optional[str]
+    frequency_duration: Optional[int]
 
 
 class CouponResponse(BaseModel):
@@ -19,4 +23,8 @@ class CouponResponse(BaseModel):
     amount_currency: str
     created_at: str
     expiration: str
-    expiration_duration: Optional[int]
+    expiration_date: Optional[str]
+    percentage_rate: Optional[float]
+    coupon_type: Optional[str]
+    frequency: Optional[str]
+    frequency_duration: Optional[int]
