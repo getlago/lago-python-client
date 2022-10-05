f36e00e9d0af40c11bc05e52083d45727e1844c9
Romain Sempé <romain.sempe@qonto.eu>
2022-10-05 16:13:03 +0200

    feat(groups): Add group on billable metric

--- lago_python_client/models/billable_metric.py
+++ lago_python_client/models/billable_metric.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List, Union
 
+class BillableMetricGroup(BaseModel):
+    key: Optional[str]
+    values: Optional[List[Union[str, dict]]]
 
 class BillableMetric(BaseModel):
     name: Optional[str]
@@ -8,7 +11,7 @@ class BillableMetric(BaseModel):
     description: Optional[str]
     aggregation_type: Optional[str]
     field_name: Optional[str]
-
+    group: Optional[BillableMetricGroup]
 
 class BillableMetricResponse(BaseModel):
     lago_id: str
@@ -18,3 +21,4 @@ class BillableMetricResponse(BaseModel):
     aggregation_type: Optional[str]
     field_name: Optional[str]
     created_at: str
+    group: BillableMetricGroup
