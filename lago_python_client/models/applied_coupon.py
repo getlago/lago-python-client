b9227420e3065ea4ed03406a70f70f48d3684d12
LovroColic <lovro.colic.lc@gmail.com>
2022-11-23 10:15:51 +0100

    feat (coupons): add support for coupon changes (#44)

--- lago_python_client/models/applied_coupon.py
+++ lago_python_client/models/applied_coupon.py
@@ -16,9 +16,11 @@ class AppliedCouponResponse(BaseModel):
     lago_id: str
     lago_coupon_id: str
     coupon_code: str
+    status: Optional[str]
     external_customer_id: str
     lago_customer_id: str
     amount_cents: int
+    amount_cents_remaining: Optional[int]
     amount_currency: str
     expiration_date: Optional[str]
     created_at: str
@@ -26,3 +28,4 @@ class AppliedCouponResponse(BaseModel):
     percentage_rate: Optional[float]
     frequency: Optional[str]
     frequency_duration: Optional[int]
+    frequency_duration_remaining: Optional[int]
