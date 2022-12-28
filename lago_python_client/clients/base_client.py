cf2e7ec4c58a080a40f0182f8882fe535fa23300
Alexander Ovchinnikov <alexander@entropia.us>
2022-12-28 12:48:16 +0300

    Add status_code, url, response, detail and headers arguments to LagoApiError exception class

--- lago_python_client/clients/base_client.py
+++ lago_python_client/clients/base_client.py
@@ -1,10 +1,14 @@
-import requests
+from collections.abc import MutableMapping
 import json
-
-from pydantic import BaseModel
-from requests import Response
-from typing import Dict
+from http import HTTPStatus
+from typing import Any
 from urllib.parse import urljoin, urlencode
+
+import orjson
+from pydantic import BaseModel
+import requests
+from requests import Response
+
 from lago_python_client.version import LAGO_VERSION
 
 
@@ -15,7 +19,7 @@ class BaseClient:
         self.base_url = base_url
         self.api_key = api_key
 
-    def find(self, resource_id: str, params: Dict = None):
+    def find(self, resource_id: str, params: dict | None = None):
         api_resource = self.api_resource() + '/' + resource_id
         query_url = urljoin(self.base_url, api_resource)
 
@@ -28,7 +32,7 @@ class BaseClient:
 
         return self.prepare_response(data)
 
-    def find_all(self, options: Dict = None):
+    def find_all(self, options: dict | None = None):
         if options:
             api_resource = self.api_resource() + '?' + urlencode(options)
         else:
@@ -64,7 +68,7 @@ class BaseClient:
         else:
             return self.prepare_response(data.json().get(self.root_name()))
 
-    def update(self, input_object: BaseModel, identifier: str = None):
+    def update(self, input_object: BaseModel, identifier: str | None = None):
         api_resource = self.api_resource()
 
         if identifier is not None:
@@ -91,19 +95,28 @@ class BaseClient:
 
         return headers
 
-    def handle_response(self, response: Response):
+    def handle_response(self, response: Response) -> Response | None:
         if response.status_code in BaseClient.RESPONSE_SUCCESS_CODES:
             if response.text:
                 return response
             else:
                 return None
         else:
+            if response.text:
+                response_data: Any = orjson.loads(response.text)
+                detail: str | None = getattr(response_data, 'error', None)
+            else:
+                response_data = None
+                detail = None
             raise LagoApiError(
-                "URI: %s. Status code: %s. Response: %s." % (
-                    response.request.url, response.status_code, response.text)
+                status_code=response.status_code,
+                url=response.request.url,
+                response=response_data,
+                detail=detail,
+                headers=response.headers,
             )
 
-    def prepare_index_response(self, data: Dict):
+    def prepare_index_response(self, data: dict):
         collection = []
 
         for el in data[self.api_resource()]:
@@ -118,4 +131,22 @@ class BaseClient:
 
 
 class LagoApiError(Exception):
-    ...
+    def __init__(
+        self,
+        status_code: int,
+        url: str | None,
+        response: Any,
+        detail: str | None = None,
+        headers: MutableMapping[str, str] | None = None,
+    ) -> None:
+        if detail is None:
+            detail = HTTPStatus(status_code).phrase
+        self.status_code = status_code
+        self.url = url
+        self.response = response
+        self.detail = detail
+        self.headers = headers
+
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"{class_name}(status_code={self.status_code!r}, detail={self.detail!r})"
