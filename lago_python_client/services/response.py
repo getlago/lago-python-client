cd6b8b0e4693f71d2257404687b0ce55f68c6596
Alexander Ovchinnikov <alexander@entropia.us>
2023-02-28 22:32:49 +0300

    Avoid walrus usage to fix compatibility with old Python versions

--- lago_python_client/services/response.py
+++ lago_python_client/services/response.py
@@ -27,11 +27,11 @@ def _is_content_exists(response: Response) -> bool:
 def verify_response(response: Response) -> Optional[Response]:
     """Verify response."""
     if not _is_status_code_successful(response):
-        response_data: Any
+        response_data: Any = from_json(response)
         raise LagoApiError(
             status_code=response.status_code,
             url=response.request.url,
-            response=(response_data := from_json(response)),
+            response=response_data,
             detail=getattr(response_data, 'error', None),
             headers=response.headers,
         )
