c2769a75323a7c3adc0b36101b366f981aadfda0
Alexander Ovchinnikov <alexander@entropia.us>
2023-03-09 23:38:25 +0300

    Use `callable_cached_property` decorator

--- lago_python_client/client.py
+++ lago_python_client/client.py
@@ -1,20 +1,23 @@
-from lago_python_client.clients.applied_add_on_client import AppliedAddOnClient
-from lago_python_client.clients.applied_coupon_client import AppliedCouponClient
-from lago_python_client.clients.billable_metric_client import BillableMetricClient
-from lago_python_client.clients.coupon_client import CouponClient
-from lago_python_client.clients.group_client import GroupClient
-from lago_python_client.clients.credit_note_client import CreditNoteClient
-from lago_python_client.clients.plan_client import PlanClient
-from lago_python_client.clients.add_on_client import AddOnClient
-from lago_python_client.clients.organization_client import OrganizationClient
-from lago_python_client.clients.subscription_client import SubscriptionClient
-from lago_python_client.clients.customer_client import CustomerClient
-from lago_python_client.clients.invoice_client import InvoiceClient
-from lago_python_client.clients.event_client import EventClient
-from lago_python_client.clients.webhook_client import WebhookClient
-from lago_python_client.clients.wallet_client import WalletClient
-from lago_python_client.clients.wallet_transaction_client import WalletTransactionClient
 from urllib.parse import urljoin
+
+from .clients.applied_add_on_client import AppliedAddOnClient
+from .clients.applied_coupon_client import AppliedCouponClient
+from .clients.billable_metric_client import BillableMetricClient
+from .clients.coupon_client import CouponClient
+from .clients.group_client import GroupClient
+from .clients.credit_note_client import CreditNoteClient
+from .clients.plan_client import PlanClient
+from .clients.add_on_client import AddOnClient
+from .clients.organization_client import OrganizationClient
+from .clients.subscription_client import SubscriptionClient
+from .clients.customer_client import CustomerClient
+from .clients.invoice_client import InvoiceClient
+from .clients.event_client import EventClient
+from .clients.webhook_client import WebhookClient
+from .clients.wallet_client import WalletClient
+from .clients.wallet_transaction_client import WalletTransactionClient
+from .functools_ext import callable_cached_property
+
 try:
     from typing import Final
 except ImportError:  # Python 3.7
@@ -33,50 +36,66 @@ class Client:
     def base_api_url(self) -> str:
         return urljoin(self.api_url if self.api_url else Client.BASE_URL, Client.API_PATH)
 
+    @callable_cached_property
     def events(self) -> EventClient:
         return EventClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def groups(self) -> GroupClient:
         return GroupClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def subscriptions(self) -> SubscriptionClient:
         return SubscriptionClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def credit_notes(self) -> CreditNoteClient:
         return CreditNoteClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def customers(self) -> CustomerClient:
         return CustomerClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def invoices(self) -> InvoiceClient:
         return InvoiceClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def applied_coupons(self) -> AppliedCouponClient:
         return AppliedCouponClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def applied_add_ons(self) -> AppliedAddOnClient:
         return AppliedAddOnClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def billable_metrics(self) -> BillableMetricClient:
         return BillableMetricClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def coupons(self) -> CouponClient:
         return CouponClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def plans(self) -> PlanClient:
         return PlanClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def add_ons(self) -> AddOnClient:
         return AddOnClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def organizations(self) -> OrganizationClient:
         return OrganizationClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def webhooks(self) -> WebhookClient:
         return WebhookClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def wallets(self) -> WalletClient:
         return WalletClient(self.base_api_url, self.api_key)
 
+    @callable_cached_property
     def wallet_transactions(self) -> WalletTransactionClient:
         return WalletTransactionClient(self.base_api_url, self.api_key)
