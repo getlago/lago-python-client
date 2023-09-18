55dd295bb70b200bf981df8cf35e39f8381c813d
William Mwai <willymwaid92@gmail.com>
2023-09-18 11:17:12 +0300

    fix: allow both `lago_item_id` and `lago_id`

--- lago_python_client/models/invoice_item.py
+++ lago_python_client/models/invoice_item.py
@@ -8,4 +8,5 @@ class InvoiceItemResponse(BaseResponseModel):
     code: Optional[str]
     name: Optional[str]
     lago_id: Optional[str]
+    lago_item_id: Optional[str]
     item_type: Optional[str]
