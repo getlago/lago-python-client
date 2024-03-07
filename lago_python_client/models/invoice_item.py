c4023619021d8729543faaba82766edbca54732b
Vincent Pochet <vincent@getlago.com>
2024-03-07 11:59:39 +0100

    feat(filter): Add filter fields

--- lago_python_client/models/invoice_item.py
+++ lago_python_client/models/invoice_item.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from ..base_model import BaseResponseModel
 
@@ -10,6 +10,8 @@ class InvoiceItemResponse(BaseResponseModel):
     lago_id: Optional[str]
     invoice_display_name: Optional[str]
     group_invoice_display_name: Optional[str]
+    filter_invoice_display_name: Optional[str]
+    filters: Optional[Dict[str, List[str]]]
     lago_item_id: Optional[str]
     item_type: Optional[str]
     grouped_by: Optional[Dict[str, str]]
