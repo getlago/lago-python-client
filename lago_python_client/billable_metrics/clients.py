a5449900dca7c4b4a0eb46f9ab69f990131cab5e
Vincent Pochet <vincent.pochet@gmail.com>
2024-11-04 12:01:49 +0100

    feat(felixible aggregation): Expose expression in API (#281)
    * feat(felixible aggregation): Expose expression in API
    
    * feat(felixible aggregation): Add documentation for expression evaluation endpoint

--- lago_python_client/billable_metrics/clients.py
+++ lago_python_client/billable_metrics/clients.py
@@ -8,7 +8,13 @@ from ..mixins import (
     FindCommandMixin,
     UpdateCommandMixin,
 )
-from ..models.billable_metric import BillableMetricResponse
+from ..models.billable_metric import (
+    BillableMetricEvaluateExpression,
+    BillableMetricEvaluateExpressionResponse,
+    BillableMetricResponse,
+)
+from ..services.request import make_headers, make_url, send_post_request
+from ..services.response import Response, prepare_object_response, get_response_data
 
 
 class BillableMetricClient(
@@ -22,3 +28,19 @@ class BillableMetricClient(
     API_RESOURCE: ClassVar[str] = "billable_metrics"
     RESPONSE_MODEL: ClassVar[Type[BillableMetricResponse]] = BillableMetricResponse
     ROOT_NAME: ClassVar[str] = "billable_metric"
+
+    def evaluate_expression(
+        self, input_object: BillableMetricEvaluateExpression
+    ) -> BillableMetricEvaluateExpressionResponse:
+        api_response: Response = send_post_request(
+            url=make_url(
+                origin=self.base_url,
+                path_parts=(self.API_RESOURCE, "evaluate_expression"),
+            ),
+            headers=make_headers(api_key=self.api_key),
+        )
+
+        return prepare_object_response(
+            response_model=BillableMetricEvaluateExpressionResponse,
+            data=get_response_data(response=api_response, key="expression_result"),
+        )
