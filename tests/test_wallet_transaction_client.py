242941d96ed47d7397f2609269c263d9144b5e70
Alexander Chebotarov <alexander.0072@gmail.com>
2025-04-08 15:20:41 +0300

    Generate payment URL for wallet transactions

--- tests/test_wallet_transaction_client.py
+++ tests/test_wallet_transaction_client.py
@@ -28,6 +28,14 @@ def mock_collection_response():
         return wallet_transaction_index_response.read()
 
 
+def mock_payment_url_response():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(this_dir, "fixtures/wallet_transaction_payment_url.json")
+
+    with open(data_path, "rb") as payment_url_response:
+        return payment_url_response.read()
+
+
 def test_valid_create_wallet_transaction_request(httpx_mock: HTTPXMock):
     client = Client(api_key="886fe239-927d-4072-ab72-6dd345e8dd0d")
 
@@ -72,3 +80,16 @@ def test_valid_find_all_wallet_transactions_request(httpx_mock: HTTPXMock):
 
     assert response["wallet_transactions"][0].lago_id == "b7ab2926-1de8-4428-9bcd-779314ac1111"
     assert response["meta"]["current_page"] == 1
+
+
+def test_valid_payment_url_request(httpx_mock: HTTPXMock):
+    client = Client(api_key="886fe239-927d-4072-ab72-6dd345e8dd0d")
+
+    httpx_mock.add_response(
+        method="POST",
+        url="https://api.getlago.com/api/v1/wallet_transactions/5eb02857-a71e-4ea2-bcf9-57d3a41bc6ba/payment_url",
+        content=mock_payment_url_response(),
+    )
+    response = client.wallet_transactions.payment_url("5eb02857-a71e-4ea2-bcf9-57d3a41bc6ba")
+
+    assert response == "https://checkout.stripe.com/c/pay/cs_test_a1cuqFkXvH"
