8d24296dc27b02e89b70c11cc9bd6a197bdf95d9
Julia <j.e.egorova@gmail.com>
2026-02-11 19:29:33 +0400

    Add customer wallet alerts

--- lago_python_client/customers/wallet_client.py
+++ lago_python_client/customers/wallet_client.py
@@ -11,6 +11,7 @@ from ..mixins import (
 )
 from ..models.wallet import WalletResponse
 from .wallets.metadata_client import CustomerWalletMetadataClient
+from .wallets.alert_client import CustomerWalletAlertClient
 
 
 class CustomerWalletClient(
@@ -35,3 +36,7 @@ class CustomerWalletClient(
     @callable_cached_property
     def metadata(self) -> CustomerWalletMetadataClient:
         return CustomerWalletMetadataClient(self.base_url, self.api_key)
+
+    @callable_cached_property
+    def alerts(self) -> CustomerWalletAlertClient:
+        return CustomerWalletAlertClient(self.base_url, self.api_key)
